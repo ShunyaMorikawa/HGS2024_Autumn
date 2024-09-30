15e098ac74074a924a11c204ae665478d965c57a
SeigaSouma <221120@st.yoshida-g.ac.jp>
2024-09-30 10:44:37 +0900

    オブジェクトの登場退場

--- 00_ソースファイル/HGS2024_Autumn/code/stageobj.h
+++ 00_ソースファイル/HGS2024_Autumn/code/stageobj.h
@@ -1,36 +1,48 @@
 //=============================================================================
 // 
-//  サンプル_オブジェクトXヘッダー [sample_objX.h]
+//  ステージの配置物ヘッダー [stageobj.h]
 //  Author : 相馬靜雅
 // 
 //=============================================================================
 
-#ifndef _SAMPLE_OBJX_H_
-#define _SAMPLE_OBJX_H_		// 二重インクルード防止
+#ifndef _STAGEOBJ_H_
+#define _STAGEOBJ_H_		// 二重インクルード防止
 
-#include "objectX.h"
+#include "object.h"
 #include "listmanager.h"
+
+// 前方宣言
+class CModel;
 
 //==========================================================================
 // クラス定義
 //==========================================================================
-// サンプル_オブジェクトXクラス
-class CSample_ObjX : public CObjectX
+// ステージの配置物クラス
+class CStageObj : public CObject
 {
 public:
 	
 	//=============================
 	// 列挙型定義
 	//=============================
-	enum Sample
+	enum State
 	{
-		SAMPLE_WAO = 0,
-		SAMPLE_WHO,
-		SAMPLE_MAX
+		STATE_NONE = 0,		// 無し
+		STATE_APPEARANCE,	// 登場
+		STATE_LEAVE,		// 退場
+		STATE_MAX
 	};
 
-	CSample_ObjX(int nPriority = 6);
-	~CSample_ObjX();
+	// 種類
+	enum Type
+	{
+		TYPE_BG = 0,	// 背景
+		TYPE_OBSTACLE,	// 障害物
+		TYPE_MAX
+	};
+
+	CStageObj(int nPriority = 6);
+	~CStageObj();
 
 	//=============================
 	// オーバーライド関数
@@ -44,38 +56,43 @@ public:
 	// メンバ関数
 	//=============================
 	void Kill();		// 削除
+	void SetState(const State& state);	// 状態設定
 
 	//=============================
 	// 静的関数
 	//=============================
 	/**
 	@brief		生成処理
-	@details	必要があれば引数追加
+	@param	type	[in]	種類
+	@param	pos		[in]	位置
 	*/
-	static CSample_ObjX *Create();
-	static CListManager<CSample_ObjX> GetListObj() { return m_List; }	// リスト取得
+	static CStageObj *Create(const Type& type, const MyLib::Vector3& pos);
+	static CListManager<CStageObj> GetList() { return m_List; }	// リスト取得
 
 private:
 
 	//=============================
 	// 関数リスト
 	//=============================
-	typedef void(CSample_ObjX::*SAMPLE_FUNC)();
-	static SAMPLE_FUNC m_SampleFuncList[];	// 関数のリスト
+	typedef void(CStageObj::*SAMPLE_FUNC)();
+	static SAMPLE_FUNC m_SampleFuncList[];	// 状態関数のリスト
 
 	//=============================
 	// メンバ関数
 	//=============================
 	// 状態関数
-	void SampleWho();	// フー
-	void SampleWao();	// ワオ
+	void UpdateState();		// 状態更新
+	void StateNone();		// なし
+	void StateAppearance();	// 登場
+	void StateLeave();		// 退場
 
 	//=============================
 	// メンバ変数
 	//=============================
-	float m_fStateTime;				// 状態カウンター
-	Sample m_state;					// 状態
-	static CListManager<CSample_ObjX> m_List;	// リスト
+	float m_fStateTime;		// 状態カウンター
+	State m_state;			// 状態
+	CModel* m_pModel;		// モデルポインタ
+	static CListManager<CStageObj> m_List;	// リスト
 
 };
 
