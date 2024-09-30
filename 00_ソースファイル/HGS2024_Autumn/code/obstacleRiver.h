055d5e9d155fb51f0dee57da4f2f2808cb87485f
RitsukiObara <221134@st.yoshida-g.ac.jp>
2024-09-30 10:40:09 +0900

    障害物の子クラスの基盤完成

--- 00_ソースファイル/HGS2024_Autumn/code/obstacleRiver.h
+++ 00_ソースファイル/HGS2024_Autumn/code/obstacleRiver.h
@@ -1,1 +1,35 @@
-#pragma once
+//========================================
+//
+// 障害物(川)の処理[obstacleRiver.h]
+// Author：小原立暉
+//
+//========================================
+#ifndef _OBSTACLE_RIVER_H_
+#define _OBSTACLE_RIVER_H_
+
+#include "obstacle.h"
+
+//========================================
+// 障害物(川)クラス
+//========================================
+class CObstacleRiver : public CObstacle
+{
+public:
+
+	CObstacleRiver();		// コンストラクタ
+	~CObstacleRiver();		// デストラクタ
+
+	// メンバ関数
+	HRESULT Init(const EType type);
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+	bool Collision(D3DXVECTOR3& rPos, const D3DXVECTOR3& rSize) override;	// プレイヤーとの当たり判定
+
+private:
+
+
+};
+
+#endif
